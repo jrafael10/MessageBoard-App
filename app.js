f4bf8284cb6e4e4682be914fb7af21130079fcda
Rafael Dizon <jesserafael102193@gmail.com>
2019-05-21 14:56:49 -0500

    updated files

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ credentials.mongo.development.password + '@' +
 credentials.mongo.development.hosts + '/' +
 credentials.mongo.development.database +
 credentials.mongo.development.options;       */
-mongoose.connect( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || connectionString);
+mongoose.connect(connectionString);
 
 
 
